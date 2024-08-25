4d94e2dfe4b72c4394520c566a41cc55f955e090
abarkera4 <abarkera4@gmail.com>
2024-08-25 17:04:34 -0500

    Further Vite Config

--- helldivers-tactician/vite.config.js
+++ helldivers-tactician/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/<repository-name>/", // Replace <repository-name> with your repository name
+  base: "/helldivers-tactician/", // Replace <repository-name> with your repository name
   plugins: [react()],
 });
