13401e9f9db5d134362fc7aea7803610dfb7946d
abarkera4 <abarkera4@gmail.com>
2024-08-25 17:48:00 -0500

    Image Path Changes

--- helldivers-tactician/vite.config.js
+++ helldivers-tactician/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "", // Ensure base is empty or set to '/' to avoid path issues
+  base: "/helldivers-tactician/", // Replace <repository-name> with your actual repo name
 });
