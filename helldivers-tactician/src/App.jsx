8e3f93789791e4d58849aff461a2de212316cc9c
abarkera4 <abarkera4@gmail.com>
2024-08-26 12:46:31 -0500

    Back to Browser

--- helldivers-tactician/src/App.jsx
+++ helldivers-tactician/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import WeaponsPage from "./pages/WeaponsPage";
 import WeaponDetail from "./pages/WeaponDetail";
