d6c2e30352e9347802cb485a91709b39f377c109
abarkera4 <abarkera4@gmail.com>
2024-08-25 19:52:15 -0500

    Revert to gh-pages

--- helldivers-tactician/vite.config.js
+++ helldivers-tactician/vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensure this is correct
   build: {
-    outDir: "dist", // Ensure the output directory is correctly set
+    outDir: "dist",
+    emptyOutDir: true,
   },
+  base: "/helldivers-tactician/", // Replace with your GitHub repo name
 });
